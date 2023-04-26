313046614cfa32199489f04cd639e43c7426ae5f
Vladlens Kukjans <kukjanvladlen@gmail.com>
2023-04-26 08:50:19 +0100

    Initial Commit

---
+++ CitiesViewSwiftUI/CitiesViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitiesViewSwiftUIApp.swift
+//  CitiesViewSwiftUI
+//
+//  Created by Vladlens Kukjans on 26/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CitiesViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
